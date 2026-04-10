606091481c1764e4710fdf9866847667ca908f60
Harsh pal <palharsh8860282160@gmail.com>
2026-04-10 16:45:49 +0530

    fix(env): update environment variables and restore commented backend API URL fix(vite): add server proxy configuration for API and auth endpoints

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,4 +9,16 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
+      '/auth': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      }
+    }
+  }
 });
